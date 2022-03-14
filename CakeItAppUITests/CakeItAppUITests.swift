cce4b2b2488c1d3ec24b56a3b70b12a6591bc5eb
MJUDDIN01 <mjuddin88@gmail.com>
2022-03-14 13:23:17 +0000

    Final commit with added UItests

---
+++ CakeItAppUITests/CakeItAppUITests.swift
@@ -0,0 +1,25 @@
+//
+//  CakeItAppUITests.swift
+//  CakeItAppUITests
+//
+//  Created by Jasim Uddin on 14/03/2022.
+//
+
+import XCTest
+class CakeItAppUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+}
