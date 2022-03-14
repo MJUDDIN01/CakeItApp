770650f19727d8e9ad944582f78ac10ed90763f6
MJUDDIN01 <mjuddin88@gmail.com>
2022-03-14 11:18:31 +0000

    Added the Model for the app

---
+++ CakeItApp/Model/Cake.swift
@@ -0,0 +1,14 @@
+//
+//  Cake.swift
+//  CakeItApp
+//
+//  Created by Jasim Uddin on 14/03/2022.
+//
+
+import Foundation
+
+struct Cake: Codable {
+    let title: String
+    let desc: String
+    let image: String
+}
